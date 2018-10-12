8917b1deba9514cc6feb4ad7aa380b1f124856ad
Schuyler Ankele <schuyler.ankele@charter.com>
2018-10-12 03:31:27 -0600

    Firefox wont inject js

--- firey_fox.rb
+++ firey_fox.rb
@@ -1,9 +1,12 @@
 require "selenium-webdriver"
 require "json"
 
-driver = Selenium::WebDriver.for :firefox
-driver.navigate.to "http://google.com"
-
+driver = Selenium::WebDriver.for :chrome
+driver.navigate.to "http://www.elections.alaska.gov/Core/candidatelistgenr.php"
+sleep 10
 puts driver.title
-
+list_items = driver.execute_script("return $('li')")
+list_items.each do |li|
+  puts li.text
+end
 driver.quit
